3de389f7f32f0d7d79e4b4a2fcd089752c1e8d56
Tanu Kaushik <129869652+Beyound3d@users.noreply.github.com>
2023-05-10 20:32:11 +0530

    Add files via upload

---
+++ QOC.py
@@ -0,0 +1,37 @@
+print("DESI MELODIES present----->
+oh shoe Versace jatta de kake kanniwaaliyan ni
+oh babbar sher ae full daler ae
+7 denaliyan ni
+oh shoe versace jatta de kake kanni waaliyan ni
+oh babbar sher se full daler ae
+7 denaliyan ni
+3 shuank ne patt honiye jatt de munde de(2)
+Ghodde chitte,Kudiyan brown,gaddiya kaaliyan ni(2)
+Oh shoe Versace jatte de kake kanni waaliyan ni
+oh babbar sher ae full daler ae 7 denaliyan ni
+Ghodde chitte,Kudiyan brown,gaddiya kaaliyan ni
+# .......mne gaghra dilwadiyo(4).....
+Jattan di hikka ch zor
+LOuis ton Leke dior
+GAllan na sari tu gerha tan mari tu Yaaran kolo hundi ni bore
+Dinda kithe chance hunda goriyan de dance
+Baby on my phone je tu karna romance
+Feel kre shy aa le shart lagayi
+Meri akhan vich dekh tenu krdange high
+Ni 2 vich kothi khanda motti
+yaar ne pakke assi ni rakhe
+chamche thaaliyan ni....
+Ghodde chitte,Kudiyan brown,gaddiya kaaliyan ni(4)
+# .......mne gaghra dilwadiyo(4).....
+ni kise ton patteya jana ni tessi da berr na goriye
+Je duniya teri jungle aa fer jatt sher aa goriyekise de darr na
+ koka jadd'na
+ Kathe hoge Jaani te karnaRoz diwaliyan ni
+ Roz diwaliyan ni3shaunk ne patt honiye jatt de munde de
+ 3 shaunk ne patt honiye jatt de munde de...
+ Ghodde chitte,Kudiyan brown,gaddiya kaaliyan ni(3)
+ # .......mne gaghra dilwadiyo.....
+
+
+
+")
---
+++ mysongcode.java
@@ -0,0 +1,6 @@
+class song{
+    public static void main(string []args)
+    {
+        system.out.println("i dont't know how to dance but i can learn from the internet")
+    }
+}
